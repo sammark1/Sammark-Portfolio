7c3909c0543c42437d1be8a8c5adcd7c5c30c890
Sammark1 <samaustinmark@gmail.com>
2022-06-01 17:24:39 -0400

    implemented bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './app.css'
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -6,6 +9,9 @@ function App() {
       <h1>Hello World</h1>
       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>
       <i class="bi bi-linkedin"></i>
+      <Container>
+      <Button>something</Button>
+      </Container>
     </div>
   );
 }
