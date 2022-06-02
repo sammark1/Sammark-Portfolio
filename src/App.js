5cadebf6be05ae1fb5d357150062e656a95727da
Sammark1 <samaustinmark@gmail.com>
2022-06-02 16:17:19 -0400

    added components folder, added targetblank links to github and linkedin

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>
-      <i class="bi bi-linkedin"></i>
+      <a href="https://www.linkedin.com/in/sammarkdev/" target="_blank"><i class="bi bi-linkedin"></i></a>
+      <a href="https://github.com/sammark1" target="_blank"><i class="bi bi-github"></i></a>
       <Container>
+      <a href="details.html">Go to Details</a>
       <Button>something</Button>
       </Container>
     </div>
