2c0a711053b35dc2fa001a32ece4fde1410bedf2
Sammark1 <samaustinmark@gmail.com>
2022-06-03 20:11:28 -0400

    working threejs in react

--- src/components/ThreeTest.jsx
+++ src/components/ThreeTest.jsx
@@ -1,19 +1,40 @@
 import React, {useRef, useEffect} from "react";
+import * as THREE from 'three';
 // import threejs from "./three"
-// import * as THREE from 'three';
 
 function ThreeTest() {
     const canvasRef= useRef(null)
     useEffect(() =>{
-        const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
-        context.fillStyle = '#000000';
-        context.fillRect(0,0, context.canvas.width, context.canvas.height)
+        // const canvas = canvasRef.current
+        // const context = canvas.getContext('2d')
+        // context.fillStyle = '#000000';
+        // context.fillRect(0,0, context.canvas.width, context.canvas.height)
+
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        // document.body.appendChild( renderer.domElement );
+        // use ref as a mount point of the Three.js scene instead of the document.body
+        canvasRef.current.appendChild( renderer.domElement );
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        var cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+        camera.position.z = 5;
+        var animate = function () {
+            requestAnimationFrame( animate );
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render( scene, camera );
+        };
+        animate();
+        return()=> canvasRef.current.removeChild( renderer.domElement)
     },[])
     return(
         <div>
             <h1>ThreeJS section</h1>
-            <canvas ref={canvasRef}></canvas>
+            <div ref={canvasRef}></div>
         </div>
     )
 }
