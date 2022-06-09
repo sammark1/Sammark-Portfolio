e0f8bebedd15da289e20622daa81a645f7220832
Sammark1 <samaustinmark@gmail.com>
2022-06-09 19:50:08 -0400

    project info added

--- src/components/About/Skills.jsx
+++ src/components/About/Skills.jsx
@@ -1,4 +1,5 @@
 import Container from 'react-bootstrap/Container';
+import TooltipIcon from '../TooltipIcon'
 
 
 function Skills(){
@@ -7,7 +8,8 @@ function Skills(){
             <h2 className="text-center">My Skillset</h2>
             <div className="row py-2">
                 <div className="col text-center">
-                    <h6 className="border-bottom">Python</h6>
+                    <i class="devicon-python-plain i-sk"></i>
+                    <p className="">Python</p>
                 </div>
                 <div className="col text-center">
                     <h6 className="border-bottom">JavaScript ES6</h6>
@@ -17,6 +19,7 @@ function Skills(){
                 </div>
                 <div className="col text-center">
                     <h6 className="border-bottom">CSS 3</h6>
+                    <TooltipIcon classPass={"i-project"} tooltip={"Maya"} isrc={"devicon-maya-plain"}/>
                 </div>
             </div>
             Skills: Python | JavaScript (ES6) | HTML & HTML5 | CSS & CSS3 | Bootstrap | React | Node.js | Express.js | React | Django | JSON | EJS | RESTful APIs | Mongoose | MongoDB | PostgreSQL | Git / GitHub
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,42 +16,84 @@ function Projects(props){
                                 <p className="d-flex justify-content-center text-secondary">
                                 <TooltipIcon classPass={"i-project"} tooltip={"Python"} isrc={"devicon-python-plain"}/>
                                 <TooltipIcon classPass={"i-project"} tooltip={"Django"} isrc={"devicon-django-plain"}/> 
-                                <TooltipIcon classPass={"i-project"} tooltip={"MongoDB"} isrc={"devicon-mongodb-plain"}/>
+                                <TooltipIcon classPass={"i-project"} tooltip={"PostgreSQL"} isrc={"devicon-postgresql-plain"}/>
                                 <TooltipIcon classPass={"i-project"} tooltip={"Bootstrap"} isrc={"devicon-bootstrap-plain"}/>
                                 </p>
-                                <a href="https://github.com/sammark1/Compend" target="_blank" className="btn btn-primary">Repository</a>
+                                <a href="https://github.com/sammark1/Compend" target="_blank" className="btn btn-secondary"><span className="devicon-github-original"></span> Repository</a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="row mb-4">
-                    <div className="col">
-                        <div className="card">
-                            <img src="https://static.wikia.nocookie.net/drawfee/images/9/95/Spheal-0.jpg" alt="butter"/>
+                    <div className="col-6">
+                        <div className="card mb-4">
+                            <img src="./assets/LaunchAProbe01.png" alt="butter"/>
                             <div className="card-body">
-                                <h5 className="card-title">Project Name</h5>
+                                <h2 className="card-title text-center">Launch a Probe</h2>
                                 <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, quasi. Eius odit nisi aspernatur animi corporis, incidunt dolore similique dicta ipsam repellat nobis. Veniam nesciunt minus reiciendis et dignissimos qui.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                                <p className="d-flex justify-content-center text-secondary">
+                                <TooltipIcon classPass={"i-project"} tooltip={"Python"} isrc={"devicon-python-plain"}/>
+                                <TooltipIcon classPass={"i-project"} tooltip={"Yarn"} isrc={"devicon-yarn-plain"}/>
+                                <TooltipIcon classPass={"i-project"} tooltip={"Django"} isrc={"devicon-django-plain"}/> 
+                                <TooltipIcon classPass={"i-project"} tooltip={"ThreeJS"} isrc={"devicon-threejs-original"}/>
+                                <TooltipIcon classPass={"i-project"} tooltip={"PostgreSQL"} isrc={"devicon-postgresql-plain"}/>
+                                </p>
+                                <div className="btn-group">
+                                    <a href="https://github.com/sammark1/Launch-a-Probe" target="_blank" className="btn btn-secondary"><span className="devicon-github-original"></span> Repository</a>
+                                    <a href="https://launch-a-probe.herokuapp.com/" target="_blank" className="btn btn-secondary"><span className="devicon-heroku-original"></span> Deploy</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col">
-                        <div className="card">
-                            <img src="https://static.wikia.nocookie.net/drawfee/images/9/95/Spheal-0.jpg" alt="butter"/>
+                    <div className="col-6">
+                        <div className="card mb-4">
+                            <img src="./assets/Revuu01.png" alt="butter"/>
                             <div className="card-body">
-                                <h5 className="card-title">Project Name</h5>
+                                <h2 className="card-title text-center">Revuu</h2>
                                 <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus assumenda veritatis in quod soluta, eaque enim placeat, iusto tempore cum fugit autem aspernatur beatae voluptatum, non odit. Minima, debitis dolores.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                                <p className="d-flex justify-content-center text-secondary">
+                                <TooltipIcon classPass={"i-project"} tooltip={"React"} isrc={"devicon-react-original"}/> 
+                                <TooltipIcon classPass={"i-project"} tooltip={"Express"} isrc={"devicon-express-original"}/>
+                                <TooltipIcon classPass={"i-project"} tooltip={"MongoDB"} isrc={"devicon-mongodb-plain"}/>
+                                </p>
+                                <div className="btn-group">
+                                    <a href="https://github.com/janiceh94/Revuu" target="_blank" className="btn btn-secondary"><span className="devicon-github-original"></span> Repository</a>
+                                    <a href="https://revuu.herokuapp.com/" target="_blank" className="btn btn-secondary"><span className="devicon-heroku-original"></span> Deploy</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col">
-                        <div className="card">
-                            <img src="https://static.wikia.nocookie.net/drawfee/images/9/95/Spheal-0.jpg" alt="butter"/>
+                    <div className="col-6">
+                        <div className="card mb-4">
+                            <img src="./assets/Intangicart01.png" alt="butter"/>
                             <div className="card-body">
-                                <h5 className="card-title">Project Name</h5>
+                                <h2 className="card-title text-center">Intangicart</h2>
                                 <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus assumenda veritatis in quod soluta, eaque enim placeat, iusto tempore cum fugit autem aspernatur beatae voluptatum, non odit. Minima, debitis dolores.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                                <p className="d-flex justify-content-center text-secondary"> 
+                                <TooltipIcon classPass={"i-project"} tooltip={"NodeJS"} isrc={"devicon-nodejs-plain"}/>
+                                <TooltipIcon classPass={"i-project"} tooltip={"Express"} isrc={"devicon-express-original"}/>
+                                <TooltipIcon classPass={"i-project"} tooltip={"MongoDB"} isrc={"devicon-mongodb-plain"}/>
+                                </p>
+                                <div className="btn-group">
+                                    <a href="https://github.com/sammark1/Intangicart" target="_blank" className="btn btn-secondary"><span className="devicon-github-original"></span> Repository</a>
+                                    <a href="https://intangicart.herokuapp.com/landing" target="_blank" className="btn btn-secondary"><span className="devicon-heroku-original"></span> Deploy</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="card mb-4">
+                            <img src="./assets/MLBeholder01.png" alt="butter"/>
+                            <div className="card-body">
+                                <h2 className="card-title text-center">My Little Beholder</h2>
+                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus assumenda veritatis in quod soluta, eaque enim placeat, iusto tempore cum fugit autem aspernatur beatae voluptatum, non odit. Minima, debitis dolores.</p>
+                                <p className="d-flex justify-content-center text-secondary"> 
+                                <TooltipIcon classPass={"i-project"} tooltip={"JavaScript"} isrc={"devicon-javascript-plain"}/>
+                                <TooltipIcon classPass={"i-project"} tooltip={"JQuery"} isrc={"devicon-jquery-plain"}/>
+                                <TooltipIcon classPass={"i-project"} tooltip={"CSS3"} isrc={"devicon-css3-plain"}/>
+                                <TooltipIcon classPass={"i-project"} tooltip={"HTML5"} isrc={"devicon-html5-plain"}/>
+                                </p>
+                                <a href="https://github.com/sammark1/My-Little-Beholder-JS-Adventure" target="_blank" className="btn btn-secondary"><span className="devicon-github-original"></span> Repository</a>
                             </div>
                         </div>
                     </div>
