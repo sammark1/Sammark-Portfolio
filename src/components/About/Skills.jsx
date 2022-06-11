98fa7463f8d2b34091f119f2314b860af62fb496
Sammark1 <samaustinmark@gmail.com>
2022-06-11 02:37:25 -0400

    added and styled skills section

--- src/components/About/Skills.jsx
+++ src/components/About/Skills.jsx
@@ -5,24 +5,86 @@ import TooltipIcon from '../TooltipIcon'
 function Skills(){
     return(
         <div id='skills'>
-            <h2 className="text-center">My Skillset</h2>
-            <div className="row py-2">
-                <div className="col text-center">
+            <h2 className="text-center">Developer Skills</h2>
+            <div className="row justify-content-center">
+                <div className="col-1 text-center">
                     <i class="devicon-python-plain i-sk"></i>
                     <p className="">Python</p>
                 </div>
-                <div className="col text-center">
-                    <h6 className="border-bottom">JavaScript ES6</h6>
+                <div className="col-1 text-center">
+                    <i class="devicon-javascript-plain i-sk"></i>
+                    <p className="">JavaScript (ES6)</p>
                 </div>
-                <div className="col text-center">
-                    <h6 className="border-bottom">HTML 5</h6>
+                <div className="col-1 text-center">
+                    <i class="devicon-html5-plain i-sk"></i>
+                    <p className="">HTML 5</p>
                 </div>
-                <div className="col text-center">
-                    <h6 className="border-bottom">CSS 3</h6>
-                    <TooltipIcon classPass={"i-project"} tooltip={"Maya"} isrc={"devicon-maya-plain"}/>
+                <div className="col-1 text-center">
+                    <i class="devicon-css3-plain i-sk"></i>
+                    <p className="">CSS3</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-react-original i-sk"></i>
+                    <p className="">React</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-nodejs-plain i-sk"></i>
+                    <p className="">Node.js</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-express-original i-sk"></i>
+                    <p className="">Express.js</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-django-plain i-sk"></i>
+                    <p className="">Django</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-original t-sk">EJS</i>
+                    <p className="">EJS</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-npm-original-wordmark i-sk"></i>
+                    <p className="">NPM</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-yarn-plain i-sk"></i>
+                    <p className="">Yarn</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-git-plain i-sk"></i>
+                    <p className="">Git</p>
+                </div>
+                <div className="col-1 text-center">
+                    <a href="https://github.com/sammark1" target="blank" className="text-black text-decoration-none">
+                    <i class="devicon-github-original i-sk"></i>
+                    <p className="">Github</p>
+                    </a>
                 </div>
             </div>
-            Skills: Python | JavaScript (ES6) | HTML & HTML5 | CSS & CSS3 | Bootstrap | React | Node.js | Express.js | React | Django | JSON | EJS | RESTful APIs | Mongoose | MongoDB | PostgreSQL | Git / GitHub
+            <h2 className="text-center">Designer Skills</h2>
+            <div className="row justify-content-center">
+                <div className="col-1 text-center">
+                    <i class="devicon-bootstrap-plain i-sk"></i>
+                    <p className="">Bootstrap</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-figma-plain i-sk"></i>
+                    <p className="">Figma</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-threejs-original i-sk"></i>
+                    <p className="">ThreeJS</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-maya-plain i-sk"></i>
+                    <p className="">Maya</p>
+                </div>
+                <div className="col-1 text-center">
+                    <i class="devicon-photoshop-plain i-sk"></i>
+                    <p className="">Photoshop</p>
+                </div>
+            </div>
         </div>
     )
 }
