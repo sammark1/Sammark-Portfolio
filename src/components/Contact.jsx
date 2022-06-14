d0ce3a1fd42c58888fa26aba4a836d77473dc3e3
Sammark1 <samaustinmark@gmail.com>
2022-06-14 14:40:39 -0400

    added contact icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,19 +7,19 @@ function Contact(props){
             <h2 className="text-center">How and where to find me</h2>
             <div className="row justify-content-center ">
                 <a href="https://github.com/sammark1" target="blank" className="col-1 text-center text-decoration-none">
-                    {/* <i class="devicon-github-original i-sk"></i> */}
+                    <i className="bi bi-github"></i>
                     <p className="">Github</p>
                 </a>
-                <a href="https://github.com/sammark1" target="blank" className="col-1 text-center text-decoration-none">
-                    {/* <i class="devicon-linkedin-plain i-sk"></i> */}
+                <a href="https://www.linkedin.com/in/sammarkdev/" target="blank" className="col-1 text-center text-decoration-none">
+                    <i className="bi bi-linkedin"></i>
                     <p className="">Linkedin</p>
                 </a>
                 <a href="mailto: samaustinmark@gmail.com" target="blank" className="col-1 text-center text-decoration-none">
-                    {/* <i class="devicon-plain i-sk">Email</i> */}
+                    <i className="bi bi-envelope"></i>
                     <p className="">Email me</p>
                 </a>
                 <a href="https://samuel-mark.format.com/" target="blank" className="col-1 text-center text-decoration-none">
-                    {/* <i class="devicon-plain i-sk">3D</i> */}
+                    <i className="bi bi-badge-3d"></i>
                     <p className="">3D art</p>
                 </a>
             </div>
